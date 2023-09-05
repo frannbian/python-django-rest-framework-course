42f29cc55e39c42dcc08726a7735e5ae444cd437
Franco Bianco <fbianco@tritansoftware.com>
2023-09-05 07:56:33 -0300

    Some course changes

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,108 @@
-from django.shortcuts import render
+from rest_framework import authentication, generics, permissions
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+from .permissions import IsStaffPermission
+from api.authentication import TokenAuthentication
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    authentication_classes = [
+        authentication.SessionAuthentication,
+        TokenAuthentication
+    ]
+    permission_classes = [permissions.IsAdminUser, IsStaffPermission]
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        # send a Django signal
+        
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class ProductUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+    
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+class ProductDeleteAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+           
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# class ProductMixinView(
+#     mixins.CreateModelMixin,
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     generics.GenericAPIView
+#     ):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk'
+
+#     def get(self, request, *args, **kwargs): #HTTP -> get
+#         pk = kwargs.get("pk")
+#         if pk is not None:
+#             return self.retrieve(request, *args, **kwargs)
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
+#         if content is None:
+#             content = "this is a single view doing cool stuff"
+#         serializer.save(content=content)
+
+#     # def post(): #HTTP -> post
+
+# product_mixin_view = ProductMixinView.as_view()
+
+
+
+# @api_view(['GET', 'POST'])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method = request.method  
+
+#     if method == "GET":
+#         if pk is not None:
+#             # detail view
+#             obj = get_object_or_404(Product, pk=pk)
+#             data = ProductSerializer(obj, many=False).data
+#             return Response(data)
+#         # list view
+#         queryset = Product.objects.all() 
+#         data = ProductSerializer(queryset, many=True).data
+#         return Response(data)
+
+#     if method == "POST":
+#         # create an item
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             title = serializer.validated_data.get('title')
+#             content = serializer.validated_data.get('content') or None
+#             if content is None:
+#                 content = title
+#             serializer.save(content=content)
+#             return Response(serializer.data)
+#         return Response({"invalid": "not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,19 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://127.0.0.1:8000/api"
+auth_endpoint = "http://localhost:8000/api/auth/" 
+username = input("What is your username?\n")
+password = getpass("What is your password?\n")
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password}) 
+print(auth_response.json())
 
-print(get_response.text)
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = "http://localhost:8000/api/products/" 
+
+    get_response = requests.get(endpoint, headers=headers) 
+    print(get_response.json())

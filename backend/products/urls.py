6eed062be17ba30e15419343f0e15dd6abbe227b
Franco Bianco <fbianco@tritansoftware.com>
2023-09-06 08:29:35 -0300

    Some changes

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from . import views
+from . import views 
 
+# /api/products/
 urlpatterns = [
-     path('', views.ProductListCreateAPIView.as_view()),
-     path('<int:pk>/update', views.ProductUpdateAPIView.as_view()),
-     path('<int:pk>/delete', views.ProductDeleteAPIView.as_view()),
-     path('<int:pk>', views.ProductDetailAPIView.as_view()),
+    path('', views.product_list_create_view),
+    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/delete/', views.product_destroy_view),
+    path('<int:pk>/', views.product_detail_view)
 ]

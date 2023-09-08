ca5b8da8bb42ee9a61eb11b34a8566875b159f80
Franco Bianco <fbianco@tritansoftware.com>
2023-09-08 07:56:36 -0300

    Changes

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,23 +1,48 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
+
+from api.serializers import UserPublicSerializer
 
 from .models import Product
+from . import validators
+
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk',
+            read_only=True
+    )
+    title = serializers.CharField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    my_discount = serializers.SerializerMethodField(read_only=True)
+    owner = UserPublicSerializer(source='user', read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+            view_name='product-detail',
+            lookup_field='pk'
+    )
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
     class Meta:
         model = Product
         fields = [
+            'owner',
+            'url',
+            'edit_url',
+            'pk',
             'title',
             'content',
             'price',
             'sale_price',
-            'my_discount',
         ]
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
     
-    def get_my_discount(self, obj):
-        if not hasattr(obj, 'id'):
+    def get_edit_url(self, obj):
+        request = self.context.get('request') # self.request
+        if request is None:
             return None
-        if not isinstance(obj, Product):
-            return None
-        return obj.get_discount()
+        return reverse("product-edit", kwargs={"pk": obj.pk}, request=request) 
